1a5305131b9bf8701d11c20f2c738e3a8c9fadee
Pavel Tashev <p.d.tashev@gmail.com>
2016-03-31 09:53:07 +0100

    Update GooglePlaces.php

--- src/GooglePlaces.php
+++ src/GooglePlaces.php
@@ -360,7 +360,7 @@ class GooglePlaces
         if ($this->isFormatJson()) {
             return json_decode($response);
         } elseif ($this->isFormatXml()) {
-            return (array) new SimpleXMLElement($response);
+            return (array) new \SimpleXMLElement($response);
         }
 
         return $response;
